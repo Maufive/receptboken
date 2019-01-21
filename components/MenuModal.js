7c1edfc76d11624ef485991e26ced6224679c03d
Niklas Albinsson <albinssonniklas@gmail.com>
2019-01-21 17:00:03 +0100

    Rensat gammal kod

--- components/MenuModal.js
+++ components/MenuModal.js
@@ -28,7 +28,7 @@ const ModalStyles = {
 		bottom: "auto",
 		marginRight: "-50%",
 		transform: "translate(-50%, -50%)",
-		background: "rgba(21, 189, 118, 0.8)",
+		background: "rgba(78, 183, 49, 0.75)",
 		width: "100vw",
 		height: "100vh",
 		zIndex: 10
